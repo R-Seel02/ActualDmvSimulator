c02a418f62ed384d7ae8968b93cd02d7cad68efb
R-Seel02 <Ryanjseely@icloud.com>
2024-03-05 18:37:52 -0500

    added more wait time and the java.random

--- TheDmvSimulator.java
+++ TheDmvSimulator.java
@@ -1,4 +1,4 @@
-
+import java.util.Random;andom.*;
 
 public class TheDmvSimulator{
 
@@ -6,11 +6,11 @@ public class TheDmvSimulator{
         System.out.println("Welcome to the DMV, grab a ticket and wait in line, boy there is a long line today");
 
         Random random = new Random();
-        int userNumber = random.nextInt(100) + 1;
+        int userNumber = random.nextInt(200) + 1;
 
         System.out.println("Your number is " + userNumber + " Sir stop yelling and wait in line");
 
-        for (int i = userNumber + 1; i <= 100; i++) {
+        for (int i = userNumber + 1; i <= 200; i++) {
             System.out.println("Calling number " + i);
         }
         for (int i = 1; i < userNumber; i++) {
